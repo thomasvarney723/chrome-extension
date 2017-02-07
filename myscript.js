dfcb8aa51ac7b53a16529fa31a41ee4b0311ef6f
Thomas Varney <thomasvarney723@gmail.com>
2017-02-07 08:58:10 -0600

    adding myscript.js and stats.html

--- myscript.js
+++ myscript.js
@@ -1,9 +1,31 @@
-var li = document.createElement("li");
-var a = document.createElement("a");
-li.appendChild(a);
-li.children[0].innerText = "STATS";
+function urlSortOrder(url) {
+    const last2URLChars = url(-2);
+    if(last2URLChars === "=1")
+        return 1;
+    else if(last2URLChars === "-1")
+        return -1;
+    else
+        console.log("error branch!!!");
+}
 
-var nav = document.getElementById("partnersNav");
-var navUl = nav.children[0];
-var lastNavElem = navUl.children[4];
-navUl.insertBefore(li, lastNavElem);
+var statsHTML =
+'<li class="" id="stats"> \
+  <a href="chrome-extension://obimnpegdkedbnankaghlkbblffejlph/stats.html">STATS</a> \
+</li>';
+document.querySelector("#partnersNav li:nth-child(4)")
+  .insertAdjacentHTML('afterend', statsHTML);
+
+var sortLabelHTML =
+  '<label for="sortDD" class="control-label">Sort order:</label>';
+  
+var sortSelectHTML =
+  '<select id="sortDD" class="form-control" onchange="javascript:submitItemsPerPageDropdown(\'invoiceSearch.html\', \'20\', \'1\', \'INVOICE_NUM_SORT\', this.value);"> \
+			<option value="1" >Ascending</option> \
+			<option value="-1">Descending</option> \
+		</select>';
+		
+document.querySelector("#resultsPerPage select")
+  .insertAdjacentHTML('afterend', sortLabelHTML + sortSelectHTML);
+  
+// invoice URL:
+// https://partners.menard-inc.com/partners/prepareHaulerInvoice.html
